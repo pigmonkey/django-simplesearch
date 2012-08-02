91b4fd9c898778f39350efeae5b7f034fe8cab45
Pig Monkey <pm@pig-monkey.com>
2012-08-02 15:10:07 -0700

    PEP8

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-simplesearch',
-    packages = find_packages(),
-    version = '1.0',
-    description = 'A basic search application for Django.',
-    author = 'Julien Phalip, Peter Hogg',
-    author_email = 'peter@havenaut.net',
-    url = 'https://github.com/pigmonkey/django-simplesearch',
-    classifiers = [
+    name='django-simplesearch',
+    packages=find_packages(),
+    version='1.0',
+    description='A basic search application for Django.',
+    author='Julien Phalip, Peter Hogg',
+    author_email='peter@havenaut.net',
+    url='https://github.com/pigmonkey/django-simplesearch',
+    classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
     ],
-    long_description = open('README.md').read(),
-    include_package_data = True,
+    long_description=open('README.md').read(),
+    include_package_data=True,
     zip_safe=True,
 )
